3cef3a68c525c0f67c820b8f4a58bf2310cd3176
postgres <postgres@en4119641m.cidse.dhcp.asu.edu>
2018-10-05 19:14:22 -0700

    ongoingCF

--- CFCosineSim.py
+++ CFCosineSim.py
@@ -5,6 +5,7 @@ import QueryRecommender as QR
 from bitmap import BitMap
 from __future__ import division
 import math
+import heapq
 
 def OR(sessionSummary, curQueryIntent):
     assert sessionSummary.size() == curQueryIntent.size()
@@ -45,55 +46,102 @@ def refineSessionSummaries(sessID, configDict, curQueryIntent, sessionSummaries,
             sessionSummaries[sessID] = ADD(sessionSummaries[sessID],curQueryIntent)
     else:
         sessionSummaries[sessID] = curQueryIntent
-    return sessionSummaries
+    return (sessionDict, sessionSummaries)
 
-def computeBitCosineSimilarity(curQueryIntent, sessionSummary):
-    nonzeroDimsQuery = curQueryIntent.nonzero() # set of all 1-bit dimensions in curQueryIntent
-    nonzeroDimsSess = sessionSummary.nonzero() # set of all 1-bit dimensions in sessionSummary
-    numSetBitsIntersect = len(list(set(nonzeroDimsQuery) & set(nonzeroDimsSess)))  # number of overlapping one bit dimensions
-    l2NormProduct = math.sqrt(len(nonzeroDimsQuery)) * math.sqrt(len(nonzeroDimsSess))
+def computeBitCosineSimilarity(curSessionSummary, oldSessionSummary):
+    nonzeroDimsCurSess = curSessionSummary.nonzero() # set of all 1-bit dimensions in curQueryIntent
+    nonzeroDimsOldSess = oldSessionSummary.nonzero() # set of all 1-bit dimensions in sessionSummary
+    numSetBitsIntersect = len(list(set(nonzeroDimsCurSess) & set(nonzeroDimsOldSess)))  # number of overlapping one bit dimensions
+    l2NormProduct = math.sqrt(len(nonzeroDimsCurSess)) * math.sqrt(len(nonzeroDimsOldSess))
     cosineSim = float(numSetBitsIntersect)/l2NormProduct
     return cosineSim
 
-def computeWeightedCosineSimilarity(curQueryIntent, sessionSummary):
-    queryDims = curQueryIntent.split(";")
-    sessDims = sessionSummary.split(";")
-    assert len(queryDims) == len(sessDims)
+def computeWeightedCosineSimilarity(curSessionSummary, oldSessionSummary):
+    curSessDims = curSessionSummary.split(";")
+    oldSessDims = oldSessionSummary.split(";")
+    assert len(curSessDims) == len(oldSessDims)
     numerator = 0.0
     l2NormQuery = 0.0
     l2NormSession = 0.0
-    for i in range(len(queryDims)):
-        numerator = numerator + float(queryDims[i] * sessDims[i])
-        l2NormQuery = l2NormQuery + float(queryDims[i]*queryDims[i])
-        l2NormSession = l2NormSession + float(sessDims[i] * sessDims[i])
+    for i in range(len(curSessDims)):
+        numerator = numerator + float(curSessDims[i] * oldSessDims[i])
+        l2NormQuery = l2NormQuery + float(curSessDims[i]*curSessDims[i])
+        l2NormSession = l2NormSession + float(oldSessDims[i] * oldSessDims[i])
     cosineSim = numerator / (math.sqrt(l2NormQuery) * math.sqrt(l2NormSession))
     return cosineSim
 
-def predictTopKIntents(sessionSummaries, curQueryIntent, configDict):
-    maxSessSim = 0.0
-    for sessIndex in sessionSummaries:
-        sessionSummary = sessionSummaries[sessIndex]
+def findTopKSessIndex(topCosineSim, cosineSimDict, topKSessindices):
+    if topCosineSim not in cosineSimDict:
+        print "cosineSimilarity not found in the dictionary !!"
+        sys.exit(0)
+    for sessIndex in cosineSimDict[topCosineSim]:
+        if sessIndex not in topKSessindices:
+            return sessIndex
+
+def predictTopKIntents(sessionSummaries, sessID, curQueryIntent, configDict):
+    # python supports for min-heap not max-heap so negate items and insert into min-heap
+    minheap = []
+    curSessionSummary = sessionSummaries[sessID]
+    cosineSimDict = {}
+    for sessIndex in range(len(sessionSummaries)-1): # exclude the current session
+        oldSessionSummary = sessionSummaries[sessIndex]
         if configDict['BIT_OR_WEIGHTED']=='BIT':
-            cosineSim = computeBitCosineSimilarity(curQueryIntent, sessionSummary)
+            cosineSim = computeBitCosineSimilarity(curSessionSummary, oldSessionSummary)
         elif configDict['BIT_OR_WEIGHTED']=='WEIGHTED':
-            cosineSim = computeWeightedCosineSimilarity(curQueryIntent, sessionSummary)
-        if cosineSim > maxSessSim:
-            maxSessSim = cosineSim
+            cosineSim = computeWeightedCosineSimilarity(curSessionSummary, oldSessionSummary)
+        heapq.heappush(minheap, -cosineSim)  # insert -ve cosineSim
+        if cosineSim not in cosineSimDict:
+            cosineSimDict[cosineSim] = list()
+        cosineSimDict[cosineSim].append(sessIndex)
+
+    topKSessIndices = []
+    for i in range(int(cosineSim['TOP_K'])):
+        topCosineSim = 0-(heapq.heappop(minheap)) # negated to get back the item
+        topKSessIndex = findTopKSessIndex(topCosineSim, cosineSimDict, topKSessIndices)
+        topKSessIndices.append(topKSessIndex)
+
+def checkEpisodeCompletion(startEpisode, configDict):
+    timeElapsed = time.time() - startEpisode
+    if timeElapsed > configDict['EPISODE_IN_SECONDS']:
+        startEpisode = time.time()
+        return (True, startEpisode)
+    else:
+        return (False, startEpisode)
+
+def retrieveSessIDQueryIDIntent(line, configDict):
+    tokens = line.split(";")
+    sessQueryName = tokens[0]
+    sessID = int(sessQueryName.split(",")[0].split(" ")[1])
+    queryID = int(sessQueryName.split(",")[1].split(" ")[1]) - 1  # coz queryID starts from 1 instead of 0
+    curQueryIntent = ';'.join(tokens[2:])
+    if ";" not in curQueryIntent and configDict['BIT_OR_WEIGHTED'] == 'BIT':
+        curQueryIntent = BitMap.fromstring(curQueryIntent)
+    else:
+        curQueryIntent = normalizeWeightedVector(curQueryIntent)
+    return (sessID, queryID, curQueryIntent)
+
+def refineSessionSummariesForAllQueriesSetAside(queryLinesSetAside, configDict, sessionDict):
+    for line in queryLinesSetAside:
+        (sessID, queryID, curQueryIntent) = retrieveSessIDQueryIDIntent(line, configDict)
+        (sessionDict, sessionSummaries) = refineSessionSummaries(sessID, configDict, curQueryIntent, sessionSummaries, sessionDict)
+    return (sessionDict, sessionSummaries)
 
 def runCFCosineSim(intentSessionFile, configDict):
     sessionSummaries = {} # key is sessionID and value is summary
     sessionDict = {} # key is session ID and value is a list of query intent vectors; no need to store the query itself
-    predictedIntents = [] # list of top-K predictions
+    numEpisodes = 0
+    queryLinesSetAside = []
+    startEpisode = time.time()
     with open(intentSessionFile) as f:
         for line in f:
-            tokens = line.split(";")
-            sessQueryName = tokens[0]
-            sessID = int(sessQueryName.split(",")[0].split(" ")[1])
-            queryID = int(sessQueryName.split(",")[1].split(" ")[1])-1 # coz queryID starts from 1 instead of 0
-            curQueryIntent = ';'.join(tokens[2:])
-            if ";" not in curQueryIntent and configDict['BIT_OR_WEIGHTED']=='BIT':
-                curQueryIntent = BitMap.fromstring(curQueryIntent)
+            (episodeDone, startEpisode) = checkEpisodeCompletion(startEpisode, configDict)
+            if episodeDone:
+                numEpisodes += 1
+                sessionSummaries = refineSessionSummariesForAllQueriesSetAside(queryLinesSetAside, configDict, sessionDict)
             else:
-                curQueryIntent = normalizeWeightedVector(curQueryIntent)
-            sessionSummaries = refineSessionSummaries(sessID, configDict, curQueryIntent, sessionSummaries, sessionDict)
-            topKPredictedIntents = predictTopKIntents(sessionSummaries, curQueryIntent, configDict)
+                queryLinesSetAside.append(line)
+            (sessID, queryID, curQueryIntent) = retrieveSessIDQueryIDIntent(line, configDict)
+            if len(sessionSummaries)>0:
+                topKPredictedIntents = predictTopKIntents(sessionSummaries, sessID, curQueryIntent, configDict)
+            else:
+                topKPredictedIntents = None
