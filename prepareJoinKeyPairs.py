906757a075c5ee3fdafd6242c20eb6402193d922
postgres <postgres@en4119641m.cidse.dhcp.asu.edu>
2018-11-16 17:25:38 -0700

    joinPairsMinc

--- prepareJoinKeyPairs.py
+++ prepareJoinKeyPairs.py
@@ -39,9 +39,73 @@ def createTableDict(cnx):
         index+=1
     return tableDict
 
+def createTabColDict(cnx, tableDict):
+    tabColDict = {}
+    tabColTypeDict = {}
+    # key is tableID and value is list of name,type
+    for table in tableDict:
+        tabID = tableDict[table]
+        query = "desc "+table
+        cursor = cnx.cursor()
+        cursor.execute(query)
+        #desc query o/p has 5 fields: field, type, null, key, default, extra
+        colList = []
+        colTypeList = []
+        for cols in cursor:
+            colName = str(cols[0])
+            colType = str(cols[1])
+            colList.append(colName)
+            colTypeList.append(colType)
+        tabColDict[tabID] = colList
+        tabColTypeDict[tabID] = colTypeList
+    return (tabColDict, tabColTypeDict)
+
+def createSelfJoinPairs(tabIndex, tabColDict, joinPairDict):
+    curTabID = tabColDict.keys()[tabIndex]
+    # key is curTabID,curTabID and value is a list of columns paired to themselves
+    joinPairDict[str(curTabID)+","+str(curTabID)] = []
+    for colName in tabColDict[curTabID]:
+        joinPairDict[str(curTabID) + "," + str(curTabID)].append(colName+","+colName)
+    return joinPairDict
+
+def createCrossJoinPairs(tabIndex, tabColDict, tabColTypeDict, joinPairDict):
+    curTabID = tabColDict.keys()[tabIndex]
+    curTabCols = tabColDict[tabIndex]
+    curTabColTypes = tabColTypeDict[tabIndex]
+    nextTabIndex = tabIndex+1
+    while nextTabIndex < len(tabColDict):
+        nextTabID = tabColDict.keys()[nextTabIndex]
+        nextTabCols = tabColDict[nextTabIndex]
+        nextTabColTypes = tabColTypeDict.keys()[nextTabIndex]
+        joinPairDict[str(curTabID)+","+str(nextTabID)] = []
+        for curTabColName in curTabCols:
+            curTabColIndex = curTabCols.index(curTabColName)
+            curTabColType = curTabColTypes[curTabColIndex]
+            for nextTabColName in nextTabCols:
+                nextTabColIndex = nextTabCols.index(nextTabColName)
+                nextTabColType = nextTabColTypes[nextTabColIndex]
+                if curTabColType == nextTabColType:
+                    joinPairDict[str(curTabID) + "," + str(nextTabID)].append(curTabColName+","+nextTabColName)
+        nextTabIndex+=1
+    return joinPairDict
+
+def createJoinPairDict(tabColDict, tabColTypeDict):
+    joinPairDict = {}
+    # key is the table ID pair and value is the list of possible join key pairs, we store both self join and cross-table join.
+    # if in the future query a join predicate has tab2.Col2 = tab1.Col3, it will always be chronologically stored as tab1.Col3 = tab2.Col2
+    # we restrict self-joins to be on the same col. That is a reasonable assumption & will confine the dimensionality.
+    tabIndex = 0
+    while tabIndex < len(tabColDict):
+        joinPairDict = createSelfJoinPairs(tabIndex, tabColDict, joinPairDict)
+        joinPairDict = createCrossJoinPairs(tabIndex, tabColDict, tabColTypeDict, joinPairDict)
+        tabIndex+=1
+    return joinPairDict
+
 def fetchSchema(configDict):
     cnx = connectToMySQL(configDict)
     tableDict = createTableDict(cnx)
+    (tabColDict, tabColTypeDict) = createTabColDict(cnx, tableDict)
+    joinPairDict = createJoinPairDict(tabColDict, tabColTypeDict)
     print "connection successful"
 
 if __name__ == "__main__":
