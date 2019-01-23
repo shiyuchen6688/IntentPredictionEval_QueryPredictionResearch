2dee71bf6692889f55e3ef4640c9544ef4830d8d
postgres <vamsikrishna1902@gmail.com>
2019-01-23 13:38:10 -0700

    RNN

--- LSTM_RNN_Parallel.py
+++ LSTM_RNN_Parallel.py
@@ -520,13 +520,10 @@ def appendResultsToFile(resultDict, elapsedAppendTime, numEpisodes, outputIntent
 
 def updateResultsToExcel(configDict, episodeResponseTimeDictName, outputIntentFileName):
     accThres = float(configDict['ACCURACY_THRESHOLD'])
-    
+
+    '''
     QR.evaluateQualityPredictions(outputIntentFileName, configDict, accThres,
                                   configDict['ALGORITHM'] + "_" + configDict['RNN_BACKPROP_LSTM_GRU'])
-    #print "--Completed Quality Evaluation for accThres:" + str(accThres)
-    #QR.evaluateTimePredictions(episodeResponseTimeDictName, configDict,
-                               #configDict['ALGORITHM'] + "_" + configDict["RNN_BACKPROP_LSTM_GRU"])
-
     outputEvalQualityFileName = getConfig(configDict['OUTPUT_DIR']) + "/OutputEvalQualityShortTermIntent_" + configDict[
         'ALGORITHM'] + "_" + configDict['RNN_BACKPROP_LSTM_GRU'] + "_" + configDict['INTENT_REP'] + "_" + configDict[
                                     'BIT_OR_WEIGHTED'] + "_TOP_K_" + configDict['TOP_K'] + "_EPISODE_IN_QUERIES_" + \
@@ -538,6 +535,11 @@ def updateResultsToExcel(configDict, episodeResponseTimeDictName, outputIntentFi
                              'RNN_INCREMENTAL_OR_FULL_TRAIN'] + ".xlsx"
     ParseResultsToExcel.parseQualityFileWithEpisodeRep(outputEvalQualityFileName, outputExcelQuality, configDict)
     '''
+
+    print "--Completed Quality Evaluation for accThres:" + str(accThres)
+    QR.evaluateTimePredictions(episodeResponseTimeDictName, configDict,
+                               configDict['ALGORITHM'] + "_" + configDict["RNN_BACKPROP_LSTM_GRU"])
+
     outputEvalTimeFileName = getConfig(configDict['OUTPUT_DIR']) + "/OutputEvalTimeShortTermIntent_" + configDict[
         'ALGORITHM'] + "_" + configDict["RNN_BACKPROP_LSTM_GRU"] + "_" + configDict['INTENT_REP'] + "_" + configDict[
                                  'BIT_OR_WEIGHTED'] + "_TOP_K_" + configDict['TOP_K'] + "_EPISODE_IN_QUERIES_" + \
@@ -547,7 +549,7 @@ def updateResultsToExcel(configDict, episodeResponseTimeDictName, outputIntentFi
                               'BIT_OR_WEIGHTED'] + "_TOP_K_" + configDict['TOP_K'] + "_EPISODE_IN_QUERIES_" + \
                           configDict['EPISODE_IN_QUERIES'] + "_" + configDict['RNN_INCREMENTAL_OR_FULL_TRAIN'] + ".xlsx"
     ParseResultsToExcel.parseTimeFile(outputEvalTimeFileName, outputExcelTimeEval)
-    '''
+
 
     print "--Completed Quality and Time Evaluation--"
     return
