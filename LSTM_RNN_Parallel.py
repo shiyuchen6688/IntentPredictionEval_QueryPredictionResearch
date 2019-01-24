1c49517ec2ffb7c9e1a64efe8b58d00e985fcd97
postgres <vamsikrishna1902@gmail.com>
2019-01-24 13:20:22 -0700

    minc

--- LSTM_RNN_Parallel.py
+++ LSTM_RNN_Parallel.py
@@ -763,6 +763,21 @@ def runFromExistingOutput(configDict):
                                configDict['INTENT_REP'] + "_" + \
                                configDict['BIT_OR_WEIGHTED'] + "_TOP_K_" + configDict[
                                    'TOP_K'] + "_EPISODE_IN_QUERIES_" + \
+                               configDict['EPISODE_IN_QUERIES']
+        episodeResponseTimeDictName = getConfig(configDict['OUTPUT_DIR']) + "/ResponseTimeDict_" + configDict[
+            'ALGORITHM'] + "_" + configDict["RNN_BACKPROP_LSTM_GRU"] + "_" + configDict['INTENT_REP'] + "_" + \
+                                      configDict['BIT_OR_WEIGHTED'] + "_TOP_K_" + configDict[
+                                          'TOP_K'] + "_EPISODE_IN_QUERIES_" + configDict[
+                                          'EPISODE_IN_QUERIES'] + ".pickle"
+        updateResultsToExcel(configDict, episodeResponseTimeDictName, outputIntentFileName)
+
+def runFromExistingOutputInBetween(configDict):
+    if configDict['SINGULARITY_OR_KFOLD'] == 'SINGULARITY':
+        outputIntentFileName = getConfig(configDict['OUTPUT_DIR']) + "/OutputFileShortTermIntent_" + \
+                               configDict['ALGORITHM'] + "_" + configDict["RNN_BACKPROP_LSTM_GRU"] + "_" + \
+                               configDict['INTENT_REP'] + "_" + \
+                               configDict['BIT_OR_WEIGHTED'] + "_TOP_K_" + configDict[
+                                   'TOP_K'] + "_EPISODE_IN_QUERIES_" + \
                                configDict['EPISODE_IN_QUERIES']+"_copy"
         episodeResponseTimeDictName = getConfig(configDict['OUTPUT_DIR']) + "/ResponseTimeDict_" + configDict[
             'ALGORITHM'] + "_" + configDict["RNN_BACKPROP_LSTM_GRU"] + "_" + configDict['INTENT_REP'] + "_" + \
