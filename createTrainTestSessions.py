68ec083c8bf5f615861c18c73f94384105b6a64f
postgres <vamsikrishna1902@gmail.com>
2019-01-29 16:13:15 -0700

    minc

--- createTrainTestSessions.py
+++ createTrainTestSessions.py
@@ -31,6 +31,8 @@ def createIntentVectors(testSessNamesFold, foldID, configDict, sessNames, intent
     for sessName in sessNames:
         sessID = int(sessName.split(" ")[1])
         numSessQueries = sessionLengthDict[sessID]
+        if sessID == 30:
+            print "hi"
         for queryID in range(numSessQueries):
             lineToOutput = sessionLineDict[str(sessID)+","+str(queryID)]
             if sessName in testSessNamesFold:
