193fdb4d5b32809201ecf7f114677daeac5bfe40
postgres <vamsikrishna1902@gmail.com>
2019-03-06 15:19:58 -0700

    minc

--- runActiveLearning.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-python ActiveLearning_Parallel.py -config MINC_configFile.txt
-#python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.2_Top_3_Last_3.txt
-#python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.4_Top_3_Last_3.txt
-#python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.6_Top_3_Last_3.txt
-#python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.8_Top_3_Last_3.txt
-#sudo shutdown
