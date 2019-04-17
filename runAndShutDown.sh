8ecfbcd793c310aa12b5d1916d139e53a039b81b
postgres <vamsikrishna1902@gmail.com>
2019-04-16 17:58:58 -0700

    cf

--- runAndShutDown.sh
+++ runAndShutDown.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-python CFCosineSim_Parallel.py -config MINC_configFile.txt
+python CF_SVD.py -config MINC_configFile.txt
+#python CFCosineSim_Parallel.py -config MINC_configFile.txt
 #python ActiveLearning_Parallel.py -config MINC_configFile.txt
 #python LSTM_RNN_Parallel.py -config MINC_configFile.txt
 #python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.2_Top_3_Last_3.txt
