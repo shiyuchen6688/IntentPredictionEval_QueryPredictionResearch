672394c92b139ffb69fe7d1cb1dd4e8409790335
postgres <vamsikrishna1902@gmail.com>
2019-05-01 14:15:38 -0700

    cf

--- MINC_FragmentIntent.py
+++ MINC_FragmentIntent.py
@@ -12,6 +12,39 @@ import random
 import ReverseEnggQueries
 #import MINC_QueryParser as MINC_QP
 
+def concatenateSeqIntentVectorFilesConcat(configDict):
+    splitDir = getConfig(configDict['BIT_FRAGMENT_SEQ_SPLITS'])
+    splitFileName = configDict['BIT_FRAGMENT_SEQ_SPLIT_NAME_FORMAT']
+    numFiles = int(configDict['BIT_FRAGMENT_SEQ_SPLIT_THREADS'])
+    sessionQueryDict = {} # key is session ID and value is line
+    queryCount = 0
+    prevSessName = None
+    #sessID = int(configDict['BIT_FRAGMENT_START_SESS_INDEX'])-1
+    sessID = -1
+    for i in range(numFiles):
+        fileNamePerThread = splitDir+"/"+splitFileName+str(i)
+        with open(fileNamePerThread) as f:
+            for line in f:
+                line = line.strip()
+                if len(line.split(";")) > 3:
+                    line = removeExcessDelimiters(line)
+                assert len(line.split(";")) == 3
+                tokens = line.split(";")
+                sessName = tokens[0].split(", ")[0].split(" ")[1]
+                if sessName != prevSessName:
+                    sessID+=1
+                    prevSessName = sessName
+                if sessID >= int(configDict['BIT_FRAGMENT_START_SESS_INDEX']):
+                    if sessID not in sessionQueryDict:
+                        sessionQueryDict[sessID] = []
+                    sessionQueryDict[sessID].append(line)
+                    queryCount +=1
+                    #if queryCount >20000:
+                        #break
+                    if queryCount % 10000 == 0:
+                        print ("Query count so far: "+str(queryCount))
+    return sessionQueryDict
+
 def concatenateSeqIntentVectorFiles(configDict):
     splitDir = getConfig(configDict['BIT_FRAGMENT_SEQ_SPLITS'])
     splitFileName = configDict['BIT_FRAGMENT_SEQ_SPLIT_NAME_FORMAT']
@@ -128,13 +161,17 @@ def createConcurrentIntentVectors(sessionQueryDict, configDict):
 
 
 if __name__ == "__main__":
-    configDict = parseConfig.parseConfigFile("MINC_configFile.txt")
+    #configDict = parseConfig.parseConfigFile("MINC_configFile.txt")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-config", help="Config parameters file", type=str, required=True)
+    args = parser.parse_args()
+    configDict = parseConfig.parseConfigFile(args.config)
     assert configDict["BIT_OR_WEIGHTED"] == "BIT"
     fragmentIntentSessionsFile = getConfig(configDict['BIT_FRAGMENT_INTENT_SESSIONS'])
     try:
         os.remove(fragmentIntentSessionsFile)
     except OSError:
         pass
-    sessionQueryDict = concatenateSeqIntentVectorFiles(configDict)
+    sessionQueryDict = concatenateSeqIntentVectorFilesConcat(configDict)
     createQuerySessions(sessionQueryDict, configDict)
     createConcurrentIntentVectors(sessionQueryDict, configDict)
