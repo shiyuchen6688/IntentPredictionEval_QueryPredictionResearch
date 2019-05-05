a855b8187933d64b121f84cff8aaacdda344713e
postgres <vamsikrishna1902@gmail.com>
2019-05-05 15:11:39 -0700

    logs

--- analyzeLogs.py
+++ analyzeLogs.py
@@ -82,11 +82,117 @@ class nextActualOps:
         self.limit = None
         self.joinPreds = None
 
-def parseLineAddOp(line, evalOpsObj, actualOrPredObj):
-    print "hi"
+def parseLineAddOp(line, actualOrPredObj):
+    if line.startswith("Query Type"):
+        actualOrPredObj.queryType = line.strip().split(": ")[1]
+    elif line.startswith("Limit"):
+        actualOrPredObj.limit = line.strip().split(": ")[1]
+    elif line.startswith("Tables"):
+        actualOrPredObj.tables = eval(line.strip().split(": ")[1])
+    elif line.startswith("Projected"):
+        actualOrPredObj.projCols = eval(line.strip().split(": ")[1])
+    elif line.startswith("AVG"):
+        actualOrPredObj.avgCols = eval(line.strip().split(": ")[1])
+    elif line.startswith("MIN"):
+        actualOrPredObj.minCols = eval(line.strip().split(": ")[1])
+    elif line.startswith("MAX"):
+        actualOrPredObj.maxCols = eval(line.strip().split(": ")[1])
+    elif line.startswith("SUM"):
+        actualOrPredObj.sumCols = eval(line.strip().split(": ")[1])
+    elif line.startswith("COUNT"):
+        actualOrPredObj.countCols = eval(line.strip().split(": ")[1])
+    elif line.startswith("SEL"):
+        actualOrPredObj.selCols = eval(line.strip().split(": ")[1])
+    elif line.startswith("GROUP"):
+        actualOrPredObj.groupByCols = eval(line.strip().split(": ")[1])
+    elif line.startswith("ORDER"):
+        actualOrPredObj.orderByCols = eval(line.strip().split(": ")[1])
+    elif line.startswith("HAVING"):
+        actualOrPredObj.havingCols = eval(line.strip().split(": ")[1])
+    elif line.startswith("JOIN"):
+        actualOrPredObj.joinPreds = eval(line.strip().split(": ")[1])
+    return
+
+def updateMetricDict(metricDict, key, val, numEpQueries):
+    if key not in metricDict:
+        metricDict[key] = val
+    else:
+        metricDict[key] = float(metricDict[key]+val)/float(numEpQueries)
+    return
+
+def computeOpF1(predOpList, actualOpList):
+    if (actualOpList is None and predOpList is not None) or\
+            (predOpList is None and actualOpList is not None):
+        return (0.0, 0.0, 0.0)
+    elif predOpList is not None and actualOpList is not None:
+        TP = len(set(predOpList).intersection(set(actualOpList)))
+        FP = len(set(predOpList) - set(actualOpList))
+        FN = len(set(actualOpList) - set(predOpList))
+        P = float(TP)/float(TP+FP)
+        R = float(TP)/float(TP+FN)
+        if P == 0.0 or R == 0.0:
+            F = 0.0
+        else:
+            F = 2*P*R / float(P+R)
+        return (P, R, F)
+    else:
+        return (None, None, None)
+
+def updateOpMetrics(P, R, F, evalOpsP, evalOpsR, evalOpsF, evalOpsObj):
+    if P is not None and R is not None and F is not None:
+        updateMetricDict(evalOpsP, evalOpsObj.curEpisode, P, evalOpsObj.numEpQueries)
+        updateMetricDict(evalOpsR, evalOpsObj.curEpisode, R, evalOpsObj.numEpQueries)
+        updateMetricDict(evalOpsF, evalOpsObj.curEpisode, F, evalOpsObj.numEpQueries)
+    return
+
+def compUpdateCondSelMetrics(evalOpsObj):
+    try:
+        if evalOpsObj.tablesF[evalOpsObj.curEpisode] == 1.0 and evalOpsObj.curEpisode in evalOpsObj.selColsP \
+                and evalOpsObj.curEpisode in evalOpsObj.selColsR and evalOpsObj.curEpisode in evalOpsObj.selColsF:
+            updateOpMetrics(evalOpsObj.selColsP, evalOpsObj.selColsR, evalOpsObj.selColsF, evalOpsObj.condSelColsP, evalOpsObj.condSelColsR, evalOpsObj.condSelColsF, evalOpsObj)
+    except:
+        pass
+
+def compUpdateOpMetrics(predOpList, actualOpList, evalOpsP, evalOpsR, evalOpsF, evalOpsObj):
+    (P,R,F) = computeOpF1(predOpList, actualOpList)
+    updateOpMetrics(P, R, F, evalOpsP, evalOpsR, evalOpsF, evalOpsObj)
+    return
 
 def computeF1(evalOpsObj, predOpsObj, nextActualOpsObj):
-    print "ji"
+    if predOpsObj.queryType == nextActualOpsObj.queryType:
+        updateOpMetrics(1.0, 1.0, 1.0, evalOpsObj.queryTypeP, evalOpsObj.queryTypeR, evalOpsObj.queryTypeF, evalOpsObj)
+    elif predOpsObj.queryType != nextActualOpsObj.queryType:
+        updateOpMetrics(0.0, 0.0, 0.0, evalOpsObj.queryTypeP, evalOpsObj.queryTypeR, evalOpsObj.queryTypeF, evalOpsObj)
+    if predOpsObj.limit == nextActualOpsObj.limit:
+        updateOpMetrics(1.0, 1.0, 1.0, evalOpsObj.limitP, evalOpsObj.limitR, evalOpsObj.limitF, evalOpsObj)
+    elif predOpsObj.limit != nextActualOpsObj.limit:
+        updateOpMetrics(0.0, 0.0, 0.0, evalOpsObj.limitP, evalOpsObj.limitR, evalOpsObj.limitF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.tables, nextActualOpsObj.tables, evalOpsObj.tablesP,
+                        evalOpsObj.tablesR, evalOpsObj.tablesF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.projCols, nextActualOpsObj.projCols, evalOpsObj.projColsP,
+                        evalOpsObj.projColsR, evalOpsObj.projColsF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.avgCols, nextActualOpsObj.avgCols, evalOpsObj.avgColsP,
+                        evalOpsObj.avgColsR, evalOpsObj.avgColsF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.minCols, nextActualOpsObj.minCols, evalOpsObj.minColsP,
+                        evalOpsObj.minColsR, evalOpsObj.minColsF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.maxCols, nextActualOpsObj.maxCols, evalOpsObj.maxColsP,
+                        evalOpsObj.maxColsR, evalOpsObj.maxColsF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.sumCols, nextActualOpsObj.sumCols, evalOpsObj.sumColsP,
+                        evalOpsObj.sumColsR, evalOpsObj.sumColsF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.countCols, nextActualOpsObj.countCols, evalOpsObj.countColsP,
+                        evalOpsObj.countColsR, evalOpsObj.countColsF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.selCols, nextActualOpsObj.selCols, evalOpsObj.selColsP,
+                        evalOpsObj.selColsR, evalOpsObj.selColsF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.groupByCols, nextActualOpsObj.groupByCols, evalOpsObj.groupByColsP,
+                        evalOpsObj.groupByColsR, evalOpsObj.groupByColsF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.orderByCols, nextActualOpsObj.orderByCols, evalOpsObj.orderByColsP,
+                        evalOpsObj.orderByColsR, evalOpsObj.orderByColsF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.havingCols, nextActualOpsObj.havingCols, evalOpsObj.havingColsP,
+                        evalOpsObj.havingColsR, evalOpsObj.havingColsF, evalOpsObj)
+    compUpdateOpMetrics(predOpsObj.joinPreds, nextActualOpsObj.joinPreds, evalOpsObj.joinPredsP,
+                        evalOpsObj.joinPredsR, evalOpsObj.joinPredsF, evalOpsObj)
+    compUpdateCondSelMetrics(evalOpsObj)
+    return
 
             
 def createEvalMetricsOpWise(evalOpsObj):
@@ -119,11 +225,12 @@ def createEvalMetricsOpWise(evalOpsObj):
                 if evalOpsObj.curQueryIndex == rank:
                     predOpsObj = nextActualOps()
             elif evalOpsObj.curQueryIndex == -1:
-                parseLineAddOp(line, evalOpsObj, nextActualOpsObj)
+                parseLineAddOp(line, nextActualOpsObj)
             elif evalOpsObj.curQueryIndex == rank:
-                parseLineAddOp(line, evalOpsObj, predOpsObj)
+                parseLineAddOp(line, predOpsObj)
             elif line.startswith("---") and predOpsObj is not None and evalOpsObj is not None:
                 computeF1(evalOpsObj, predOpsObj, nextActualOpsObj)
+            prevEpisode = evalOpsObj.curEpisode
 
 
 if __name__ == "__main__":
