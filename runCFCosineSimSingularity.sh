c1c1d6ea84cf856042e7d3a34d33153d2adda694
postgres <vamsikrishna1902@gmail.com>
2019-05-07 11:14:38 -0700

    log

--- runCFCosineSimSingularity.sh
+++ runCFCosineSimSingularity.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #python QLearning.py -config configDir/MINC_QL_sustenance_configFile.txt
 #python QLearning.py -config configDir/MINC_QL_configFile.txt
-#python CF_SVD.py -config configDir/MINC_SVD_sustenance_configFile.txt
-#python CFCosineSim_Parallel.py -config configDir/MINC_CF_COSINESIM_sustenance_configFile.txt
+#python CF_SVD.py -config MINC_configFile.txt
+python CFCosineSim_Parallel.py -config configDir/MINC_CF_COSINESIM_singularity_configFile.txt
+python CreateSQLLogs.py -config configDir/MINC_CF_COSINESIM_singularity_configFile.txt
 #python ActiveLearning_Parallel.py -config MINC_configFile.txt
-python LSTM_RNN_Parallel.py -config configDir/MINC_RNN_sustenance_configFile.txt
+#python LSTM_RNN_Parallel.py -config configDir/MINC_RNN_sustenance_configFile.txt
 #python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.2_Top_3_Last_3.txt
 #python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.4_Top_3_Last_3.txt
 #python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.6_Top_3_Last_3.txt
