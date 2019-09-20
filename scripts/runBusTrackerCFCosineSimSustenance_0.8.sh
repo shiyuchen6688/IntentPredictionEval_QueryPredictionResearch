6743986369caba5891149e67ae8ea531b4f763b2
postgres <vamsikrishna1902@gmail.com>
2019-09-20 15:07:26 -0700

    cf

---
+++ scripts/runBusTrackerCFCosineSimSustenance_0.8.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#python QLearning_selOpConst.py -config configDir/MINC_QL_sustenance_configFile.txt
+#python QLearning_selOpConst.py -config configDir/MINC_QL_configFile.txt
+#python CF_SVD_selOpConst.py -config MINC_configFile.txt
+python CFCosineSim_Parallel.py -config configDir/BusTracker_CF_COSINESIM_trainTest_sustenance_0.8_configFile.txt
+python CreateSQLLogs.py -config configDir/BusTracker_CF_COSINESIM_trainTest_sustenance_0.8_configFile.txt
+#python ActiveLearning_Parallel.py -config MINC_configFile.txt
+#python LSTM_RNN_Parallel_selOpConst.py -config configDir/MINC_RNN_sustenance_configFile.txt
+#python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.2_Top_3_Last_3.txt
+#python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.4_Top_3_Last_3.txt
+#python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.6_Top_3_Last_3.txt
+#python ActiveLearning_Parallel.py -config configDir/MINC_configFile_1Fold_Random_Sample_1.0_Incremental_Weight_0.8_Top_3_Last_3.txt
+#sudo shutdown
