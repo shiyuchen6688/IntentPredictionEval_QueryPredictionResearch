cc3e49d004f7bfcdb866fcf2c18c616bb9e1fc21
Venkata Vamsikrishna Meduri (Student) <vmeduri@sundevils.asu.edu>
2020-07-17 13:47:05 -0700

    cf

--- scripts/runHistoricalRNNSustenance_0.8_runQueries.sh
+++ scripts/runHistoricalRNNSustenance_0.8_runQueries.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python analyzeLogs_runQueries.py -config configDir/MINC_Historical_RNN_sustenance_configFile.txt -log ../Documents/DataExploration-Research/MINC/InputOutput/ClusterRuns/NovelTables-114607-Constants/sustenance/HistoricalRNN/Sustenance_HistoricalRNN_outputSQLLog
+python analyzeLogs_runQueries.py -config configDir/MINC_Historical_RNN_trainTest_sustenance_configFile.txt -log ../Documents/DataExploration-Research/MINC/InputOutput/ClusterRuns/NovelTables-114607-Constants/sustenance/HistoricalRNN/Sustenance_HistoricalRNN_outputSQLLog
 # nohup sh scripts/runHistoricalRNNSustenance_0.8_runQueries.sh > ../runHistoricalRNNSustenance_0.8_runQueries.out 2> ../runHistoricalRNNSustenance_0.8_runQueries.err &
