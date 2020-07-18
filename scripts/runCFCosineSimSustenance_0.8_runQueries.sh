551dd9b9bf69b2798600b9ba209e9130bfc2a341
Venkata Vamsikrishna Meduri (Student) <vmeduri@sundevils.asu.edu>
2020-07-17 20:39:28 -0700

    cf

--- scripts/runCFCosineSimSustenance_0.8_runQueries.sh
+++ scripts/runCFCosineSimSustenance_0.8_runQueries.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python analyzeLogs_runQueries.py -config configDir/MINC_CF_COSINESIM_trainTest_sustenance_0.8_configFile.txt -log ../Documents/DataExploration-Research/MINC/InputOutput/ClusterRuns/NovelTables-114607-Constants/sustenance/CFCosineSim/Sustenance_CF_COSINESIM_outputSQLLog
+python analyzeLogs_runQueries.py -config configDir/MINC_CF_COSINESIM_trainTest_sustenance_0.8_configFile.txt -log ../Documents/DataExploration-Research/MINC/InputOutput/ClusterRuns/NovelTables-114607-Constants/sustenance/CFCosineSim/Sustenance_CF_COSINESIM_OutputSQLLog
 # nohup sh scripts/runCFCosineSimSustenance_0.8_runQueries.sh > ../runCFCosineSimSustenance_0.8_runQueries.out 2> ../runCFCosineSimSustenance_0.8_runQueries.err &
