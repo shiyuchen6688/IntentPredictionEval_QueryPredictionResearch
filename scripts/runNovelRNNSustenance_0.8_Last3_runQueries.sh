4f884faacd98181f64dfbaabb4ca0f9986911764
Venkata Vamsikrishna Meduri (Student) <vmeduri@sundevils.asu.edu>
2020-08-12 22:57:19 -0700

    cf

---
+++ scripts/runNovelRNNSustenance_0.8_Last3_runQueries.sh
@@ -0,0 +1,3 @@
+#!/bin/sh 
+python analyzeLogs_runQueries.py -config configDir/MINC_Novel_RNN_trainTest_sustenance_0.8_configFile.txt -log ../Documents/DataExploration-Research/MINC/InputOutput/ClusterRuns/NovelTables-114607-Constants/sustenance/NovelRNN-LAST-3/Sustenance_NovelRNN_outputSQLLog
+# nohup sh scripts/runNovelRNNSustenance_0.8_Last3_runQueries.sh > ../runNovelRNNSustenance_0.8_Last3_runQueries.out 2> ../runNovelRNNSustenance_0.8_Last3_runQueries.err &
